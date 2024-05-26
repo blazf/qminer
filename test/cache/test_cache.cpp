509280917a1f4a89739517458d3af25029fb31f2
Blaz Fortuna <blaz@blazfortuna.com>
2024-05-26 06:56:08 +0200

    Convert articles to test JSON

--- test/cache/test_cache.cpp
+++ test/cache/test_cache.cpp
@@ -26,7 +26,123 @@ void ReportMemory(TStr Msg = "") {
 
 #endif
 
+void ConvertArticleToTestJson() {
+	printf("------ Converting articles to test JSON ------\n");
+	TStrV ArticleFNmV;
+	ArticleFNmV.Add("./data/2023-06-08 Articles.json");
+	ArticleFNmV.Add("./data/2023-06-09 Articles.json");
+	ArticleFNmV.Add("./data/2023-06-10 Articles.json");
+	ArticleFNmV.Add("./data/2023-06-11 Articles.json");
+	const TStr OutFNm = "./data/TestBody.json";
+
+	for (const TStr& ArticleFNm : ArticleFNmV) {
+		printf("Converting %s ...\n", ArticleFNm.CStr());
+		TFIn FIn(ArticleFNm);
+		TFOut FOut(OutFNm, true);
+		TStr Line;
+		while (FIn.GetNextLn(Line)) {
+			PJsonVal ArticleJson = TJsonVal::GetValFromStr(Line);
+			TStr BodyStr = ArticleJson->GetObjStr("Body");
+			PJsonVal TestJson = TJsonVal::NewObj();
+			TestJson->AddToObj("Text", BodyStr);
+			FOut.PutStrLn(TestJson->SaveStr());
+		}
+	}
+	printf("Done.\n");
+}
+
 void CreateTestStore() {
+	printf("------ Creating test store ------\n");
+	TQm::TEnv::InitLogger(1, "std", true);
+
+	// load schema
+    const TStr StoreJsonFNm = "./test.def";
+	if (!TFile::Exists(StoreJsonFNm)) {
+        TQm::TEnv::Logger->OnStatusFmt("could not locate the test.def file");
+        return;
+    }
+    PJsonVal StoreDefVal = TJsonVal::GetValFromStr(TStr::LoadTxt(StoreJsonFNm));
+
+	// create base
+	const uint64 StoreCacheSize = 100 * TInt::Mega;
+	const uint64 IndexCacheSize = 100 * TInt::Mega;
+	TQm::PBase Base = TQm::TStorage::NewBase("./test_base/", StoreDefVal, IndexCacheSize, StoreCacheSize,
+		true, TStrUInt64H(), TStrUInt64H(), false, 128, true);
+
+	// load articles
+	const TStr JsonFNm = "./data/TestBody.json";
+	TQm::PStore TestStore = Base->GetStoreByStoreNm("Test");
+	TQm::TEnv::Logger->OnStatusFmt("Loading %s json ...", JsonFNm.CStr());
+	TFIn FIn(JsonFNm); TStr Line;
+	uint64 LastTm = TTm::GetCurUniMSecs();
+	while (FIn.GetNextLn(Line)) {
+		PJsonVal Json = TJsonVal::GetValFromStr(Line);
+		// add record
+		const uint64 RecId = TestStore->AddRec(Json);
+		// print status
+		if (RecId % 10000 == 0) {
+			double Diff = (TTm::GetCurUniMSecs() - LastTm) / 1000.0;
+			TQm::TEnv::Logger->OnStatusFmt("%llu (%.0fs)", RecId, Diff);
+			LastTm = TTm::GetCurUniMSecs();
+		}
+	}
+	// update last time
+	double Diff = (TTm::GetCurUniMSecs() - LastTm) / 1000.0;
+	TQm::TEnv::Logger->OnStatusFmt("%llu (%.0fs)", TestStore->GetRecs(), Diff);
+
+	// save base
+	TQm::TStorage::SaveBase(Base);
+}
+
+void TestIndexCache() {
+	printf("------ Testing index cache ------\n");
+	TQm::TEnv::InitLogger(1, "std", true);
+
+	const uint64 StoreCacheSize = 100 * TInt::Mega;
+	const uint64 IndexCacheSize = 100 * TInt::Mega;
+	TWPt<TQm::TBase> Base = TQm::TStorage::LoadBase("./test_base/", faRdOnly, IndexCacheSize, StoreCacheSize);
+	ReportMemory("on_load");
+
+	// prepare list of possible words
+	TQm::PStore TestStore = Base->GetStoreByStoreNm("Test");
+	TQm::PIndexVoc IndexVoc = Base->GetIndexVoc();
+	const int KeyId = IndexVoc->GetKeyId(TestStore->GetStoreId(), "Text");
+	EAssert(IndexVoc->IsWordVoc(KeyId));
+	EAssert(IndexVoc->GetKey(KeyId).IsTextPos());
+	TStrV WordStrV; IndexVoc->GetAllWordStrV(KeyId, WordStrV);
+	printf("Text index has %d words\n", WordStrV.Len());
+	// map words to ids
+	TIntV WordIdV;
+	for (const TStr& WordStr : WordStrV) {
+		const int WordId = IndexVoc->GetWordId(KeyId, WordStr);
+		WordIdV.Add(WordId);
+	}
+
+	printf("Sleeping for 10 seconds to note down memory in task manager ...\n");
+	sleep(10);
+	printf("Waking up ...\n");
+
+	TRnd Rnd(1); uint64 Searches = 0, TotalRecords = 0;
+	while (Searches < 100*(uint64)TInt::Mega) {
+		// prepare single word query
+		// const int RndWordId = WordIdV[123];
+		const int RndWordId = WordIdV[Rnd.GetUniDevInt(WordIdV.Len())];
+		const TStr RndWordStr = IndexVoc->GetWordStr(KeyId, RndWordId);
+		const TQm::TQueryItem Item(Base, "Test", "Text", TUInt64V::GetV(RndWordId), TIntV());
+		// execute query
+		TQm::PRecSet RecSet = Base->Search(Item);
+		TotalRecords += RecSet->GetRecs();
+		// print status every 1000 searches
+		if (Searches % 100000 == 0) {
+			ReportMemory(TStr::Fmt("searches=%llu, records=%llu", Searches, TotalRecords));
+		}
+		Searches++;
+	}
+}
+
+///////////////////////////////////////////////
+
+void CreateArticleStore() {
 	printf("------ Creating test store ------\n");
 	TQm::TEnv::InitLogger(1, "std", true);
 
@@ -81,7 +197,7 @@ void CreateTestStore() {
 	TQm::TStorage::SaveBase(Base);
 }
 
-void TestStoreCache() {
+void TestArticleStoreCache() {
 	printf("------ Testing store cache ------\n");
 	TQm::TEnv::InitLogger(1, "std", true);
 
@@ -112,7 +228,7 @@ void TestStoreCache() {
 	}
 }
 
-void TestIndexCache() {
+void TestArticleIndexCache() {
 	printf("------ Testing index cache ------\n");
 	TQm::TEnv::InitLogger(1, "std", true);
 
@@ -173,15 +289,20 @@ int main(int argc, char* argv[]) {
 	}
 
     try {
-		if (Env.IsArgStr("--create")) {
+		if (Env.IsArgStr("--convert")) {
+			printf("Converting articles to test JSON ...\n");
+			ConvertArticleToTestJson();
+		} else if (Env.IsArgStr("--create")) {
 			printf("Creating test store ...\n");
 			CreateTestStore();
+			// CreateArticleStore();
 		} else if (Env.IsArgStr("--store")) {
 			printf("Testing store cache ...\n");
-			TestStoreCache();
+			// TestArticleStoreCache();
 		} else if (Env.IsArgStr("--index")) {
 			printf("Testing index cache ...\n");
 			TestIndexCache();
+			// TestArticleIndexCache();
 		} else {
 			printf("No arguments provided.\n");
 		}
